7478130aee453e9ceda770f6008b42fd7a7a0b61
Wisara8 <srandall845@gmail.com>
2018-07-05 22:27:53 +0000

    complete

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+    let sum = 0;
+  // arr = true;
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      // Print out all it's items individually
+      // let arr = true;
+      sum += sumItems(item);
+
+    } else {
+      // console.log(item);
+       sum += item;
+    }
+  });
+  return sum;
+  // return item;
 }
 
 module.exports = sumItems;
